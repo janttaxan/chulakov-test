f962e44cd824de6d7795fda763f8b08bb3b609af
Maxim Frolov <buterbrodhs@gmail.com>
2021-03-19 18:08:45 +0300

    верстка поля поиска

---
+++ src/components/Icons/IconClose.tsx
@@ -0,0 +1,13 @@
+export const IconClose = () => (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M15 2.41L13.59 1L8 6.59L2.41 1L1 2.41L6.59 8L1 13.59L2.41 15L8 9.41L13.59 15L15 13.59L9.41 8L15 2.41Z"
+      fill="#677B8F"
+    />
+  </svg>
+);
